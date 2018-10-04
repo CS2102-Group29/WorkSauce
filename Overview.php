ce79568095727589a06060a00252b6458aca93fa
Chan Heng Ming <43297847+chanhengming@users.noreply.github.com>
2018-10-04 22:25:33 +0800

    Overview Page(after login)

---
+++ Overview.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Task Sourcing Overview</title>
+<meta charset="utf-8" />
+  <link rel="shortcut icon" href="assets/images/logo.png">
+  <link href="./semantic.min.css" rel="stylesheet" type="text/css">
+  <script src="./jquery.min.js"></script>
+  <script src="./semantic.min.js"></script>
+<style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+</style>
+</head>
+<body>
+
+<div class="ui massive inverted menu">
+  <div class="item">
+    <div class="ui icon input">
+      <input type="text" placeholder="Search...">
+        <i class="search link icon"></i>
+    </div>
+  </div>
+  <div class="right menu">
+    <div class="ui simple dropdown item">
+      Task Manager <i class="dropdown icon"></i>
+      <div class="menu">
+        <a class="item">Upcoming Tasks</a>
+        <a class="item">Completed Tasks</a>
+        <a class="item">Current Bids</a>
+      </div>
+    </div>
+    <a class="item">
+      View Profile
+    </a>
+    <a class="active red item">
+      Create Task
+    </a>
+    <a class="ui item">
+      Logout
+    </a>
+  </div>
+</div>
+
+<div style="padding-left:32px">
+<h2 class="ui icon header">
+  <i class="child icon"></i>
+  <div class="content">
+    Help Someone In Need
+    <div class="sub header">Pick a task and make someone's life better.</div>
+  </div>
+</h2>
+</div>
+
+<div style="padding-left:16px">
+  <div class="ui massive list">
+    <div class="item">
+      <div class="content">
+        <a class="header">Stephane</a>
+        <div class="description">Needs help with <a><b>Wall Fixing</b></a>.</div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="content">
+        <a class="header">Aaron</a>
+        <div class="description">Needs help with <a><b>Shifting Furniture</b></a>.</div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="content">
+        <a class="header">Li Feng</a>
+        <div class="description">Needs help with <a><b>Babysitting</b></a>.</div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="content">
+        <a class="header">Sun Teck</a>
+        <div class="description">Needs help with <a><b>Painting The House</b></a>.</div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="content">
+        <a class="header">Hsiang Hui</a>
+        <div class="description">Needs help with <a><b>Shifting Furniture</b></a>.</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
