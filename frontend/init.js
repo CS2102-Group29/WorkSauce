0376ff17db1705453b73709a04e6b35a9a84629d
Ohjun Kwon <dev@kojandy.com>
2018-10-31 12:15:33 +0800

    Fix bug: location matching

--- frontend/init.js
+++ frontend/init.js
@@ -1,7 +1,7 @@
 const SERVER_URL = 'http://localhost:4000';
 let current_user = sessionStorage.getItem('current_user');
 
-if (!['/login.html', '/signup.html'].includes(window.location.pathname)) {
+if (!/\/(login|signup).html$/.test(location.href)) {
     if (current_user === null) {
         location.href ='login.html';
     }
